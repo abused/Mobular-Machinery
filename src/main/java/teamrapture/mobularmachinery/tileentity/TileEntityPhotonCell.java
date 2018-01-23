58c84fa2bc13c736ff25c71085f1f44b7ca440bd
abused_master <techperson71@gmail.com>
2018-01-23 06:29:54 -0500

    Prototype multiblock code and solar generation!

--- src/main/java/teamrapture/mobularmachinery/tileentity/TileEntityPhotonCell.java
+++ src/main/java/teamrapture/mobularmachinery/tileentity/TileEntityPhotonCell.java
@@ -1,8 +1,11 @@
 package teamrapture.mobularmachinery.tileentity;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.energy.CapabilityEnergy;
 import teamrapture.mobularmachinery.utils.CustomEnergyStorage;
@@ -12,6 +15,7 @@ import javax.annotation.Nullable;
 public class TileEntityPhotonCell extends TileEntityBase {
 
     public CustomEnergyStorage storage = new CustomEnergyStorage(10000, 10000);
+    public boolean clearSkies = false;
 
     public TileEntityPhotonCell() {
     }
@@ -20,11 +24,13 @@ public class TileEntityPhotonCell extends TileEntityBase {
     public void readFromNBT(NBTTagCompound nbt) {
         super.readFromNBT(nbt);
         storage.readFromNBT(nbt);
+        clearSkies = nbt.getBoolean("ClearSkies");
     }
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
         storage.writeToNBT(nbt);
+        nbt.setBoolean("ClearSkies", clearSkies);
         return super.writeToNBT(nbt);
     }
 
@@ -33,6 +39,26 @@ public class TileEntityPhotonCell extends TileEntityBase {
         if(!world.isRemote) {
             IBlockState state = world.getBlockState(pos);
             world.notifyBlockUpdate(pos, state, state, 3);
+        }
+
+        for (int i = 0; i < 50; i++) {
+            BlockPos blockPos = new BlockPos(pos.getX(), pos.getY() + i, pos.getZ());
+            Block block = world.getBlockState(blockPos).getBlock();
+            if(i >= 50) {
+                if(block == Blocks.AIR) {
+                    clearSkies = true;
+                }else {
+                    clearSkies = false;
+                }
+            }else {
+                if(clearSkies) {
+                   clearSkies = false;
+                }
+            }
+        }
+
+        if(clearSkies) {
+            storage.receiveEnergy(100, false);
         }
     }
 
--- src/main/java/teamrapture/mobularmachinery/tileentity/TileEntityPhotonCore.java
+++ src/main/java/teamrapture/mobularmachinery/tileentity/TileEntityPhotonCore.java
@@ -6,6 +6,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.energy.CapabilityEnergy;
+import teamrapture.mobularmachinery.registry.ModResources;
 import teamrapture.mobularmachinery.utils.CustomEnergyStorage;
 import teamrapture.mobularmachinery.utils.MobularUtils;
 import teamrapture.mobularmachinery.utils.hud.IHudSupport;
@@ -15,6 +16,7 @@ import javax.annotation.Nullable;
 public class TileEntityPhotonCore extends TileEntityBase implements IHudSupport {
 
     public CustomEnergyStorage storage = new CustomEnergyStorage(100000, 10000);
+    public boolean isMultiblock = false;
 
     public TileEntityPhotonCore() {
     }
@@ -23,11 +25,13 @@ public class TileEntityPhotonCore extends TileEntityBase implements IHudSupport 
     public void readFromNBT(NBTTagCompound nbt) {
         super.readFromNBT(nbt);
         storage.readFromNBT(nbt);
+        isMultiblock = nbt.getBoolean("Multiblock");
     }
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
         storage.writeToNBT(nbt);
+        nbt.setBoolean("Multiblock", isMultiblock);
         return super.writeToNBT(nbt);
     }
 
@@ -37,6 +41,45 @@ public class TileEntityPhotonCore extends TileEntityBase implements IHudSupport 
             IBlockState state = world.getBlockState(pos);
             world.notifyBlockUpdate(pos, state, state, 3);
         }
+        checkMultiblock();
+    }
+
+    public boolean checkMultiblock() {
+        BlockPos pos1 = new BlockPos(pos.getX() - 1, pos.getY(), pos.getZ());
+        BlockPos pos2 = new BlockPos(pos.getX() + 1, pos.getY(), pos.getZ());
+        BlockPos pos3 = new BlockPos(pos.getX(), pos.getY(), pos.getZ() - 1);
+        BlockPos pos4 = new BlockPos(pos.getX(), pos.getY(), pos.getZ() + 1);
+        BlockPos pos5 = new BlockPos(pos.getX() + 1, pos.getY(), pos.getZ() + 1);
+        BlockPos pos6 = new BlockPos(pos.getX() + 1, pos.getY(), pos.getZ() - 1);
+        BlockPos pos7 = new BlockPos(pos.getX() - 1, pos.getY(), pos.getZ() + 1);
+        BlockPos pos8 = new BlockPos(pos.getX() - 1, pos.getY(), pos.getZ() - 1);
+
+        if(world.getBlockState(pos1).getBlock() == ModResources.blockPhotonCell) {
+            if(world.getBlockState(pos2).getBlock() == ModResources.blockPhotonCell) {
+                if(world.getBlockState(pos3).getBlock() == ModResources.blockPhotonCell) {
+                    if(world.getBlockState(pos4).getBlock() == ModResources.blockPhotonCell) {
+                        if(world.getBlockState(pos5).getBlock() == ModResources.blockPhotonCell) {
+                            if(world.getBlockState(pos6).getBlock() == ModResources.blockPhotonCell) {
+                                if(world.getBlockState(pos7).getBlock() == ModResources.blockPhotonCell) {
+                                    if(world.getBlockState(pos8).getBlock() == ModResources.blockPhotonCell) {
+                                        //System.out.println("Multiblock is formed");
+                                        if(isMultiblock = false) {
+                                            isMultiblock = true;
+                                        }
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if(isMultiblock = true) {
+            isMultiblock = false;
+        }
+        return false;
     }
 
     @Override
