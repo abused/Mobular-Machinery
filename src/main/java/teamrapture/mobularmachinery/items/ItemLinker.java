a4708472f67d7d3d95e1f624d2ac3b39258e3587
abused_master <techperson71@gmail.com>
2018-01-31 17:05:28 -0500

    Finished Energy Relay, now for models/textures

--- src/main/java/teamrapture/mobularmachinery/items/ItemLinker.java
+++ src/main/java/teamrapture/mobularmachinery/items/ItemLinker.java
@@ -7,6 +7,7 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
@@ -16,6 +17,7 @@ import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import teamrapture.mobularmachinery.MobularMachinery;
 import teamrapture.mobularmachinery.registry.ModResources;
+import teamrapture.mobularmachinery.tileentity.TileEntityEnergyRelay;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -34,6 +36,7 @@ public class ItemLinker extends Item {
     public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
         ItemStack stack = player.getHeldItem(hand);
         IBlockState state = world.getBlockState(pos);
+        TileEntityEnergyRelay te = (TileEntityEnergyRelay) world.getTileEntity(pos);
         NBTTagCompound tag = stack.getTagCompound();
         if(!world.isRemote) {
 
@@ -43,7 +46,7 @@ public class ItemLinker extends Item {
 
             if (player.isSneaking()) {
                 if (tag != null) {
-                    if (state.getBlock() == ModResources.blockWaterVaporizer) {
+                    if (state.getBlock() == ModResources.blockEnergyRelay) {
                         tag.setInteger("x", pos.getX());
                         tag.setInteger("y", pos.getY());
                         tag.setInteger("z", pos.getZ());
@@ -58,6 +61,16 @@ public class ItemLinker extends Item {
                             player.sendMessage(new TextComponentString("Removed tile position!"));
                         }
                         return EnumActionResult.FAIL;
+                    }
+                }
+            } else {
+                if (tag != null && tag.hasKey("x") && tag.hasKey("y") && tag.hasKey("z")) {
+                    TileEntity tileCheck = world.getTileEntity(new BlockPos(tag.getInteger("x"), tag.getInteger("y"), tag.getInteger("z")));
+                    if (tileCheck != null && tileCheck instanceof TileEntityEnergyRelay) {
+                        if (state.getBlock() == ModResources.blockEnergyRelay) {
+                            te.setRelayPosition(stack);
+                            player.sendMessage(new TextComponentString("Linked relay positions!"));
+                        }
                     }
                 }
             }
--- src/main/java/teamrapture/mobularmachinery/tileentity/TileEntityEnergyRelay.java
+++ src/main/java/teamrapture/mobularmachinery/tileentity/TileEntityEnergyRelay.java
@@ -1,7 +1,10 @@
 package teamrapture.mobularmachinery.tileentity;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.energy.CapabilityEnergy;
 import teamrapture.mobularmachinery.utils.CustomEnergyStorage;
@@ -11,23 +14,71 @@ import javax.annotation.Nullable;
 public class TileEntityEnergyRelay extends TileEntityBase {
 
     public CustomEnergyStorage storage = new CustomEnergyStorage(5000);
+    public int x = 0;
+    public int y = 0;
+    public int z = 0;
 
     public TileEntityEnergyRelay() {
     }
 
     @Override
-    public void readFromNBT(NBTTagCompound compound) {
-        super.readFromNBT(compound);
+    public void readFromNBT(NBTTagCompound nbt) {
+        super.readFromNBT(nbt);
+        storage.readFromNBT(nbt);
+        x = nbt.getInteger("X");
+        y = nbt.getInteger("Y");
+        z = nbt.getInteger("Z");
     }
 
     @Override
-    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
-        return super.writeToNBT(compound);
+    public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
+        storage.writeToNBT(nbt);
+        nbt.setInteger("X", x);
+        nbt.setInteger("Y", y);
+        nbt.setInteger("Z", z);
+        return super.writeToNBT(nbt);
     }
 
     @Override
     public void update() {
-        super.update();
+        if(isLinked()) {
+            BlockPos linkedPos = new BlockPos(x, y, z);
+            TileEntityEnergyRelay relay = (TileEntityEnergyRelay) world.getTileEntity(linkedPos);
+            int relayStorage = relay.storage.getEnergyStored();
+            storage.setEnergyStored(relayStorage);
+            relay.storage.setEnergyStored(relayStorage);
+            handleSendingEnergy();
+            relay.handleSendingEnergy();
+        }
+    }
+
+    public boolean isLinked() {
+        if(x != 0 && y != 0 && z != 0) {
+            BlockPos linkedPos = new BlockPos(x, y, z);
+            if(world.getTileEntity(linkedPos) instanceof TileEntityEnergyRelay) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void setRelayPosition(ItemStack stack) {
+        NBTTagCompound tag = stack.getTagCompound();
+        if(tag == null) {
+            tag = new NBTTagCompound();
+        }
+
+        if(tag != null) {
+            if(tag.hasKey("x") && tag.hasKey("y") && tag.hasKey("z")) {
+                x = tag.getInteger("x");
+                y = tag.getInteger("y");
+                z = tag.getInteger("z");
+            }else {
+                x = 0;
+                y = 0;
+                z = 0;
+            }
+        }
     }
 
     @Override
@@ -42,8 +93,42 @@ public class TileEntityEnergyRelay extends TileEntityBase {
     @Override
     public <T> T getCapability(Capability<T> capability, @Nullable EnumFacing facing) {
         if(capability == CapabilityEnergy.ENERGY) {
-
+            return (T) storage;
         }
         return super.getCapability(capability, facing);
+    }
+
+    private void handleSendingEnergy() {
+        int energyStored = storage.getEnergyStored();
+
+        for (EnumFacing facing : EnumFacing.values()) {
+            BlockPos pos = getPos().offset(facing);
+            TileEntity te = world.getTileEntity(pos);
+            if (isEnergyTE(te)) {
+                EnumFacing opposite = facing.getOpposite();
+                int rfToGive = 1000 <= energyStored ? 1000 : energyStored;
+                int received;
+
+                received = receiveEnergy(te, opposite, rfToGive);
+                energyStored -= storage.extractEnergy(received, false);
+                if (energyStored <= 0) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static int receiveEnergy(TileEntity tileEntity, EnumFacing from, int maxReceive) {
+        if (tileEntity != null && tileEntity.hasCapability(CapabilityEnergy.ENERGY, from)) {
+            net.minecraftforge.energy.IEnergyStorage capability = tileEntity.getCapability(CapabilityEnergy.ENERGY, from);
+            if (capability.canReceive()) {
+                return capability.receiveEnergy(maxReceive, false);
+            }
+        }
+        return 0;
+    }
+
+    public static boolean isEnergyTE(TileEntity te) {
+        return te != null && te.hasCapability(CapabilityEnergy.ENERGY, null);
     }
 }
